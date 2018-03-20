e473b73f47b7bd7b59c861ed1a9968a0124461de
YamilGonzalez <yamil.gonzalez4@upr.edu>
2018-03-20 11:44:19 -0400

    Fixed some indentations

--- src/solutionsP/P1P2solution.java
+++ src/solutionsP/P1P2solution.java
@@ -8,10 +8,7 @@ import setIntersectionFinders.AbstractIntersectionFinder;
 
 public class P1P2solution<E> extends AbstractIntersectionFinder {
 
-	public P1P2solution(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
+	public P1P2solution(String name) {super(name);}
 
 	@Override
 	public MySet intersectSets(MySet[] t) {
@@ -38,12 +35,7 @@ public class P1P2solution<E> extends AbstractIntersectionFinder {
 				}
 			}
 			return T;
-			
 		}
-		return null;
-			
-		
+		return null;		
 	}
-
 }
-
