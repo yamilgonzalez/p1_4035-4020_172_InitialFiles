7d33729429155b8976d36db88b3afd8aa2b86b84
maria.marrero <maria.marrero@CRAIWS15.ecenet.ece.uprm.edu>
2018-03-20 12:14:23 -0400

    Fixed an error on P3

--- src/solutionsP/P3solution.java
+++ src/solutionsP/P3solution.java
@@ -15,13 +15,13 @@ public class P3solution<E> extends AbstractIntersectionFinder<E> {
 		ArrayList<E> allElements = new ArrayList<E>();
 		Integer m = t.length;
 		for(int i= 0 ; i<m ; i++){
-			for (Object x : t){
+			for (Object x : t[i]){
 				allElements.add((E) x);
 			}
 		}
 		
-		allElements.sort(null);		
-		MySet<E> T = new Set2();  // sets in P3's solution are of type Set2
+		allElements.sort(null); 		
+		MySet<E> T = new Set2<E>();  // sets in P3's solution are of type Set2
 		E e = allElements.get(0); 
 		Integer c = 1;
 		for (int i=1; i < allElements.size(); i++) {
