64c018b80e463faa3a1e5f82d9b6b769065b0925
maria.marrero <maria.marrero@INCADELWS18.ecenet.ece.uprm.edu>
2018-03-22 11:27:36 -0400

    Enhanced the creation of intersection finders

--- src/p1MainClasses/Part1Main.java
+++ src/p1MainClasses/Part1Main.java
@@ -13,7 +13,7 @@ import solutionsP.P1P2solution;
 import solutionsP.P3solution;
 import solutionsP.P4solution;
 /**
- * 
+ *
  * @author Maria A. Marrero
  * @author Yamil J. Gonzalez
  *
@@ -25,53 +25,37 @@ public class Part1Main {
 		if (args.length > 1)
 			System.out.println("Unexpected number of parameters. Must be 1.");
 		
+		//Create an array of intersection finders 
+		IntersectionFinder[] intFind = new IntersectionFinder[4]; 
+		//initialize each of them
+		intFind[0] = new P1P2solution("P1");
+		intFind[1] = new P1P2solution("P2");
+		intFind[2] = new P3solution("P3");
+		intFind[3] = new P4solution("P4"); 
+
 		if (args.length!=0) { //The user selected a strategy
-			switch(args[0]){
-			case "1": { //Strategy by P1 Selected
-				IntersectionFinder P1 = new P1P2solution("P1");
-				System.out.println("Final Set by P1:" + P1.intersectSets(unionFinder(args[0])));
-				break;
-			}
-			case "2": { //Strategy by P2 Selected
-				IntersectionFinder P2 = new P1P2solution("P2");
-				System.out.println("Final Set by P2:" + P2.intersectSets(unionFinder(args[0])));
-				break;
-			}
-			case "3": { //Strategy by P3 Selected
-				IntersectionFinder P3 = new P3solution("P3");
-				System.out.println("Final Set by P3:" + P3.intersectSets(unionFinder(args[0])));
-				break;
-			}
-			case "4": { //Strategy by P4 Selected
-				IntersectionFinder P4 = new P4solution("P4");
-				System.out.println("Final Set by P4:" + P4.intersectSets(unionFinder(args[0])));
-				break;
-			}
-			}
+			int strategy = Integer.parseInt(args[0])-1;
+			System.out.println("Final set by " + intFind[strategy].getName()+":" + intFind[strategy].intersectSets(unionFinder(args[0])));
 		}
 		else { //The user did not selected a strategy, apply all
-			IntersectionFinder P1 = new P1P2solution("P1");
-			IntersectionFinder P2 = new P1P2solution("P2");
-			IntersectionFinder P3 = new P3solution("P3");
-			IntersectionFinder P4 = new P4solution("P4");
-			System.out.println("Final Set by P1:" + P1.intersectSets(unionFinder("1")));
-			System.out.println("Final Set by P2:" + P2.intersectSets(unionFinder("2")));
-			System.out.println("Final Set by P3:" + P3.intersectSets(unionFinder("3")));
-			System.out.println("Final Set by P4:" + P4.intersectSets(unionFinder("4")));
+			for(int i = 0; i<intFind.length ; i++){
+				System.out.println("Final set by " + intFind[i].getName()+":" + intFind[i].intersectSets(unionFinder(Integer.toString(i+1))));
+			}
 		}
+
 	}
-/**
- * Reads all files and finds the union set of the numbers of each crime event and stores them in 
- * an array of sets
- * @param argument the strategy selected by user
- * @return the array of Sets, the type depends of the strategy selected
- * @throws FileNotFoundException
- */
+	/**
+	 * Reads all files and finds the union set of the numbers of each crime event and stores them in 
+	 * an array of sets
+	 * @param argument the strategy receiving the set
+	 * @return the array of Sets, the type depends of the strategy selected
+	 * @throws FileNotFoundException
+	 */
 	private static MySet[] unionFinder(String argument) throws FileNotFoundException {
 		DataReader dr = new DataReader();
 		Object [][][] dataSet = dr.readDataFiles();
 		MySet[] t = new MySet[dataSet[0].length];
-		
+
 		for(int j = 0 ; j<dataSet[0].length ; j++) {
 			t[j] = new Set2();                           //initializes the set as Set2
 			if(argument.equals("1")) t[j] = new Set1();  //if the args is "1" then it is changed to Set1
